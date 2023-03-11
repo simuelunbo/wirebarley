18f426ef9d14ad0413b473e5b49ddf16f18bc09a
simjunbo <simuelunbo@gmail.com>
2023-03-11 11:44:14 +0900

    Test: CurrencyUsecase 테스트 작성

---
+++ domain/src/test/java/com/simuel/domain/usecase/CurrencyUseCaseTest.kt
@@ -0,0 +1,30 @@
+package com.simuel.domain.usecase
+
+import com.google.common.truth.Truth
+import com.simuel.domain.model.Quote
+import com.simuel.domain.repository.CurrencyRepository
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.Test
+import org.junit.jupiter.api.DisplayName
+
+@ExperimentalCoroutinesApi
+class CurrencyUseCaseTest {
+    private val repository: CurrencyRepository = mockk()
+
+    @Test
+    @DisplayName("해당 환율 데이터를 성공적으로 가져온다.")
+    fun getQuoteSuccess() = runTest {
+        // given
+        val quote = Quote(1121.419945, 110.959498, 52.72027)
+        val useCase = GetCurrencyUseCase(repository)
+        coEvery { repository.getCurrency() } returns Result.success(quote)
+        // when
+        val actual = useCase().getOrThrow()
+
+        // then
+        Truth.assertThat(actual).isEqualTo(quote)
+    }
+}
